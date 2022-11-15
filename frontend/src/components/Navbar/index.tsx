bc7669094aaa480d3b6877ab9959552d25c06475
Patrick Santos <patrick.s.sousa@hotmail.com>
2022-11-15 00:21:03 -0300

    TUDO REFINADO

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -7,14 +7,14 @@ function Navbar() {
             <nav className="container">
                 <div className="dsmovie-nav-content">
                     <h1>QUAL SEU CUPCAKE FAVORITO ?</h1>
-                    <a href="https://github.com/Patrick-JVM">
+                   {/*  <a href="https://github.com/Patrick-JVM">
                         <div className="dsmovie-contact-container">
                             <GithubIcon />
                             <p className="dsmovie-contact-link">/DevPatrick</p>
                             <div className="dsmovie-contact-">
                             </div>
                         </div>
-                    </a>
+                    </a> */}
                 </div>
             </nav>
         </header>
