03349652022a726d20104b68bbf04171546548a1
Patrick Santos <patrick.s.sousa@hotmail.com>
2022-11-19 00:39:25 -0300

    Ajustes de Obrigatoriedade de campo

--- frontend/src/components/FormCard/index.tsx
+++ frontend/src/components/FormCard/index.tsx
@@ -57,8 +57,8 @@ function FormCard( { movieId } : Props) {
                 <h3>{movie?.title}</h3>
                 <form className="dsmovie-form" onSubmit={handleSubmit}>
                     <div className="form-group dsmovie-form-group">
-                        <label htmlFor="email">Informe seu email</label>
-                        <input type="email" className="form-control" id="email" />
+                        <label htmlFor="email">* Informe seu email:</label>
+                        <input type="email" className="form-control" id="email" required/>
                     </div>
                     <div className="form-group dsmovie-form-group">
                         <label htmlFor="score">Informe sua avaliação</label>
